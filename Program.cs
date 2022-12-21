420bea871864ebbded9d84ab8abb4e2133ac693c
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-21 23:30:59 +0530

    [Sangmeshwar]:Giving a Message, ability to analyse and respond Happy or Sad Mood

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MoodAnalyserMSTest
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("---Welcome to MoodAnalyser MSTesting---\n");
+            Console.WriteLine("Enter your mood to check");
+            string mood = Console.ReadLine();
+            MoodAnalyser obj = new MoodAnalyser();
+            obj.analyseMood(mood);
+            
+        }
+    }
+}
