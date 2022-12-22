894bce343189d96c66dd942f45de48c348d74529
Sangmeshwar Ramesh Patil <89923434+Sangmeshwar96@users.noreply.github.com>
2022-12-22 19:45:25 +0530

    [Sangmeshwar]:Informing user if entered Invalid Mood

--- MoodAnalyser.cs
+++ MoodAnalyser.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace MoodAnalyserMSTest
 {
     public class MoodAnalyser
@@ -23,14 +22,16 @@ namespace MoodAnalyserMSTest
             {
                 if (this.message.Equals(string.Empty))
                 {
-                    throw new Exception("The string is empty, you must enter something");
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
                 }
+                else if (this.message == null)
+                    throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
                 else if (this.message.ToUpper().Contains("SAD"))
                     return "SAD";
                 else
                     return "HAPPY";
             }
-            catch (Exception obj)
+            catch (CustomException obj)
             {
                 Console.WriteLine(obj.Message);
             }
